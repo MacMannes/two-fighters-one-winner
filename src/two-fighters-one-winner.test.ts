2a35057625579469efc519dec0bb087b160c6228
André Mathlener <andre.mathlener@ns.nl>
2024-12-19 11:13:20 +0100

    Added first thest

--- src/two-fighters-one-winner.test.ts
+++ src/two-fighters-one-winner.test.ts
@@ -1,9 +1,21 @@
 import { describe, expect, it } from 'vitest';
-import { TwoFightersOneWinner } from './two-fighters-one-winner';
+import { Fighter, declareWinner } from './two-fighters-one-winner';
 
 describe('Two Fighters One Winner', () => {
-    it('Creating an instance of the TwoFightersOneWinner class should work', () => {
-        const clazz = new TwoFightersOneWinner();
-        expect(clazz).toBeDefined();
+    /**
+     * fighter1 10,10
+        fighter2, 2,2
+
+        Fighter 1 wins in 1 turn
+     */
+    describe('Fighter 1 wins in 1 turn', () => {
+        it('Creating an instance of the TwoFightersOneWinner class should work', () => {
+            const fighter1 = new Fighter('Lew', 10, 10);
+            const fighter2 = new Fighter('Harry', 2, 2);
+
+            const winner = declareWinner(fighter1, fighter2, 'Lew');
+
+            expect(winner).toBe('Lew');
+        });
     });
 });
--- src/two-fighters-one-winner.ts
+++ src/two-fighters-one-winner.ts
@@ -1,2 +1,11 @@
-export class TwoFightersOneWinner {
+export class Fighter {
+    constructor(
+        private name: string,
+        private health: number,
+        private damagePerAttack: number,
+    ) {}
+}
+
+export function declareWinner(fighter1: Fighter, fighter2: Fighter, firstAttacker: string): string {
+    return 'Lew';
 }
